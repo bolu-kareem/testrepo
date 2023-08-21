6aa62dd38f577564f32ebe6b90897b5c034156d5
bolu-kareem <76807014+bolu-kareem@users.noreply.github.com>
2023-08-21 16:09:18 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,74 @@ class karid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.grey[900],
+     appBar: AppBar(
+       title: const Text('BOLT ID'),
+       centerTitle: true,
+       backgroundColor: Colors.grey[850],
+       elevation: 0.0,
+     ),
+        body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0,40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+           const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+           const SizedBox(height:10.0),
+            const Text(
+              'Kareem',
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height:30.0),
+            const Text(
+              'CODING LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height:10.0),
+            const Text(
+              '7',
+              style: TextStyle(
+                color: Colors.pinkAccent,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height:30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10.0,),
+                 const Text(
+                  'bolukareem16@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+        )
     );
   }
 }
